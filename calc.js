46d91aa17947e1d1bef149a437e825c4f94c697e
Andy <andrew.j.c.e@gmail.com>
2024-09-17 15:38:48 -0400

    added pseudo code to get started

---
+++ calc.js
@@ -0,0 +1,7 @@
+// capture a users button input (create event listeners for buttons and different button types)
+// capture button input order
+// display input within display line-2
+// if viewing a result display AC button, else show Backspace button(⌫)
+// validate input sequence, reject invalid inputs
+// write test cases for calc functions
+// display result on = (submit)
